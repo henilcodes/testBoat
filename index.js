21cc62e6ae93237b81f74e23e236f661ca6c9cf8
henilcodes <henilcode@gmail.com>
2026-02-08 10:20:11 +0530

    fix

--- index.js
+++ index.js
@@ -9,16 +9,14 @@ app.post("/whatsapp/webhook", (req, res) => {
   const incoming = (req.body.Body || "").trim().toLowerCase();
 
   const twiml = new twilio.twiml.MessagingResponse();
-  
+
   if (["hi", "hello", "hey"].includes(incoming)) {
     twiml.message(`Hello ${req.body.ProfileName}, How can I help?`);
+  } else if (["help", "support", "contact"].includes(incoming)) {
+    twiml.message(
+      "You can reach out to us at henilcode@gmail.com or call us at 9081807209.",
+    );
   } else {
-    twiml.message("I got your message ✅");
-  }
-
-  if (["help", "support", "contact"].includes(incoming)) {
-    twiml.message("You can reach out to us at henilcode@gmail.com or call us at 9090909090.");
-  }else{
     twiml.message("I got your message ✅");
   }
 
